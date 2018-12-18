eb8f4ac5b055551eb64e8a2ca5113ebd3c6ffd0c
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-12-18 13:04:11 +0530

    sched: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: Ia43c01e7e368186399b9d021a6761545c7da0c18
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>

--- kernel/sched/boost.c
+++ kernel/sched/boost.c
@@ -130,6 +130,7 @@ static void _sched_set_boost(int type)
 	case NO_BOOST: /* All boost clear */
 		if (boost_refcount[FULL_THROTTLE_BOOST] > 0) {
 			core_ctl_set_boost(false);
+			walt_enable_frequency_aggregation(false);
 			boost_refcount[FULL_THROTTLE_BOOST] = 0;
 		}
 		if (boost_refcount[CONSERVATIVE_BOOST] > 0) {
@@ -148,6 +149,8 @@ static void _sched_set_boost(int type)
 			core_ctl_set_boost(true);
 			restore_cgroup_boost_settings();
 			boost_kick_cpus();
+			if (!boost_refcount[RESTRAINED_BOOST])
+				walt_enable_frequency_aggregation(true);
 		}
 		break;
 
@@ -162,7 +165,8 @@ static void _sched_set_boost(int type)
 
 	case RESTRAINED_BOOST:
 	    boost_refcount[RESTRAINED_BOOST]++;
-		if (boost_refcount[RESTRAINED_BOOST] == 1)
+		if (boost_refcount[RESTRAINED_BOOST] == 1 &&
+		    !boost_refcount[FULL_THROTTLE_BOOST])
 			walt_enable_frequency_aggregation(true);
 		break;
 
@@ -173,6 +177,9 @@ static void _sched_set_boost(int type)
 				core_ctl_set_boost(false);
 				if (boost_refcount[CONSERVATIVE_BOOST] >= 1)
 					update_cgroup_boost_settings();
+				if (!boost_refcount[RESTRAINED_BOOST])
+					walt_enable_frequency_aggregation(
+								false);
 			}
 		}
 		break;
@@ -188,7 +195,8 @@ static void _sched_set_boost(int type)
 	case RESTRAINED_BOOST_DISABLE:
 		if (boost_refcount[RESTRAINED_BOOST] >= 1) {
 			boost_refcount[RESTRAINED_BOOST]--;
-			if (!boost_refcount[RESTRAINED_BOOST])
+			if (!boost_refcount[RESTRAINED_BOOST] &&
+			    !boost_refcount[FULL_THROTTLE_BOOST])
 				walt_enable_frequency_aggregation(false);
 		}
 		break;
