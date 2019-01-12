51b71569a91ba82bb54bdd7f9999c16c120e9db7
Bruno Martins <bgcngm@gmail.com>
2019-01-12 21:09:02 +0000

    arm64: Fix compilation with HARDEN_BRANCH_PREDICTOR config disabled
    Change-Id: Ic4fe0c6fe76fdbbb809abcbc02de31d759304b4a

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -31,6 +31,12 @@
 #include <uapi/linux/psci.h>
 #endif
 
+#if defined(CONFIG_ARM64_SSBD) || defined(CONFIG_PSCI_BP_HARDENING)
+#include <linux/arm-smccc.h>
+#include <linux/psci.h>
+#include <uapi/linux/psci.h>
+#endif
+
 static bool __maybe_unused
 is_affected_midr_range(const struct arm64_cpu_capabilities *entry, int scope)
 {
