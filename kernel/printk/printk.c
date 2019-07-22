9960492792b0252d161522403af07cc0b5384fee
palaych <palaychm@yandex.ru>
2019-07-22 23:39:53 +0300

    printk: Make ret __maybe_unused

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2846,7 +2846,7 @@ EXPORT_SYMBOL(unregister_console);
 static int __init printk_late_init(void)
 {
 	struct console *con;
-	int ret;
+	int __maybe_unused ret;
 
 	for_each_console(con) {
 		if (!keep_bootcon && con->flags & CON_BOOT) {
