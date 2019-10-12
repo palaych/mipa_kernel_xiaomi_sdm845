163416a634f63bd4926e9586ee0857962cc04a54
palaych <palaychm@yandex.ru>
2019-10-12 20:28:07 +0300

    Revert "selinux: don't require auditing"
    Include revert commits:
    
        Revert "selinux: don't require auditing"
    
        This reverts commit b3f975f396c05c11961be8702215be45e4d63da2.
    
        Revert "selinux: avc: omit audit callbacks if disabled"
    
        This reverts commit adfd3fb4edb1f440239182ed089b7a82f5b5bea0.
    
        Revert "selinux: fix unused function warnings when audit is disabled"
    
        This reverts commit e20677da78a8eebadce707a96b7a01e870c21569.

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3294,7 +3294,6 @@ out:
 	return match;
 }
 
-#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3317,7 +3316,6 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
-#endif
 
 #ifdef CONFIG_NETLABEL
 /**
