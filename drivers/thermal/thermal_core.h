38c663cd9739ae79b08d2bd9e35ff4f738b521b4
palaych <palaychm@yandex.ru>
2019-11-21 00:07:44 +0300

    drivers: thermal: Add some fix after import Xiaomi changes
    * Revert 225a4423f02eb7c5fa3fccd89108f359893479ba
    * Fix dc1684031bc395cc0631613219fffd75369daf54

--- drivers/thermal/thermal_core.h
+++ drivers/thermal/thermal_core.h
@@ -130,10 +130,10 @@ int of_thermal_aggregate_trip(struct thermal_zone_device *tz,
 			      enum thermal_trip_type type,
 			      int *low, int *high);
 void of_thermal_handle_trip(struct thermal_zone_device *tz);
-int of_parse_thermal_message(void);
-void free_thermal_message(void);
 void of_thermal_handle_trip_temp(struct thermal_zone_device *tz,
 					int trip_temp);
+int of_parse_thermal_message(void);
+void free_thermal_message(void);
 #else
 static inline int of_parse_thermal_zones(void) { return 0; }
 static inline void of_thermal_destroy_zones(void) { }
